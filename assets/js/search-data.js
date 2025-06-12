733aa6a7785d8cb0daffd273ed7cb2ec22d01d94
bje1012 <144383874+bje1012@users.noreply.github.com>
2025-06-12 22:31:51 +0000

    Deploying to gh-pages from @ bje1012/bje1012.github.io@f6596fc5be5f921b57f5d200986d0c308de88a82 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -404,7 +404,7 @@ ninja.data = [{
               window.location.href = "/projects/4_project/";
             },},{id: "projects-network-analysis-with-r",
           title: 'Network Analysis with R',
-          description: "Network Analysis using Latent Space Model (LSM)",
+          description: "Network Analysis using Latent Space Model (LSM) with R",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{
